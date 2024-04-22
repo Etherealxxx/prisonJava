9ff008902b6d6b7f82cfef4276b39b0f2d46f92f
Etherealxxx <146776331+Etherealxxx@users.noreply.github.com>
2024-04-22 12:47:17 +0300

    Add files via upload

---
+++ PrisonFunctions.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class PrisonFunctions {
+    public static void lockdown() {
+        System.out.println("Тюрьма находится в режиме полной изоляции.");
+    }
+
+    public static void visitationHours() {
+        System.out.println("Часы посещений для заключенных: с 10:00 до 12:00.");
+    }
+
+    public static void countPrisoners() {
+        int totalPrisoners = 30;
+        System.out.println("Общее количество заключенных в тюрьме: " + totalPrisoners);
+    }
+    public void exerciseYard() {
+        System.out.println("Время для прогулки.");
+    }
+
+    public void mailCall() {
+        System.out.println("Проверка почты для заключенных.");
+    }
+
+    public void paroleHearing() {
+        System.out.println("Заседание по делу удо.");
+    }
+    public void visitorRegistration() {
+        visitationHours();
+        System.out.println("Процедура регистрации посетителей.");
+    }
+
+    public void contrabandCheck() {
+        System.out.println("Проверка на контрабанду.");
+    }
+
+    public void solitaryConfinement() {
+        lockdown();
+        System.out.println("Заключение в одиночную камеру.");
+    }
+
+    public static void main(String[] args) {
+        countPrisoners();
+        lockdown();
+
+        PrisonFunctions prison = new PrisonFunctions();
+        prison.exerciseYard();
+        prison.visitorRegistration();
+    }
+}
